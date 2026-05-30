86b320a6ef3b57cc68f495015904ed2cfd5a4bde
MJ <marthinusjacobs@gmail.com>
2026-05-30 09:36:20 +0200

    Upgrade Gradle to 9.5.1 and Kotlin to 2.3.21
    Migrate build scripts to Kotlin DSL
    
    Upgrade compileSdk and targetSdk to 36
    
    Update project JDK to 21
    
    Remove legacy .iml and configuration files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,17 +4,19 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
-        applicationId ="com.refresh.pos"
-        minSdkVersion(21)
-        targetSdkVersion(34)
+        applicationId = "com.refresh.pos"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled =false
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.txt")
         }
     }
@@ -22,9 +24,9 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.gridlayout:gridlayout:1.0.0")
-    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation (files("libs/achartengine-1.1.0.jar"))
-    implementation (files("libs/android-integration-2.0-supportv4.jar"))
-    implementation("androidx.core:core-ktx:+")
+    implementation("androidx.gridlayout:gridlayout:1.1.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.core:core-ktx:1.18.0")
+    implementation(files("libs/achartengine-1.1.0.jar"))
+    implementation(files("libs/android-integration-2.0-supportv4.jar"))
 }
